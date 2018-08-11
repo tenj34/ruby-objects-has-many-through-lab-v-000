a760411a7eeedfc8f859c7d1d51f2d4e325b2d83
Juan Tenezaca <tenj34@gmail.com>
2018-08-11 18:03:48 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,14 +1,16 @@
 class Patient
   attr_accessor :name
+  @@all = []
 
   def initialize(name)
     @name = name
+
   end
 
   def new_appointment(doctor, date)
     Appointment.new(date, self, doctor)
   end
-  
+
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end

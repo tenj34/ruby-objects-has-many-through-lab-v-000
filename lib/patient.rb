746a6a9e38b70ed033b3d32272cb3ec0b71c9257
Juan Tenezaca <tenj34@gmail.com>
2018-08-11 17:58:54 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -12,11 +12,6 @@ class Patient
   def appointments
     Appointment.all.select {|appointment| appointment.patient == self}
   end
-
-  def doctors
-      appointments.map do |appointment|
-        appointment.doctor
-      end
-    end
+  
 
 end
